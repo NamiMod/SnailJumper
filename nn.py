3a890590f05bbd6bfc326867466d9ee7f6da85ac
Nami Modarressi <nami@Namis-MacBook-Pro.local>
2022-06-29 19:00:53 +0430

    NN added

--- nn.py
+++ nn.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-
 class NeuralNetwork:
+    
+    w = []
+    b = []
 
     def __init__(self, layer_sizes):
         """
@@ -10,8 +12,14 @@ class NeuralNetwork:
         :param layer_sizes: A list containing neuron numbers in each layers. For example [3, 10, 2] means that there are
         3 neurons in the input layer, 10 neurons in the hidden layer, and 2 neurons in the output layer.
         """
-        # TODO (Implement FCNNs architecture here)
-        pass
+        # initialize w and b with random numbers and 0
+
+        self.activation = np.vectorize(self.activation)
+
+        for i in range(0,len(layer_sizes) - 1):
+            NeuralNetwork.b.append(np.zeros((layer_sizes[i + 1], 1))) # [len(leyer) * 1]
+            NeuralNetwork.w.append(np.random.normal(0, 1, size=(layer_sizes[i + 1], layer_sizes[i]))) # [len(leyer[i+1]) * len(layer[i])]
+            
 
     def activation(self, x):
         """
@@ -19,8 +27,8 @@ class NeuralNetwork:
         :param x: Vector of a layer in our network.
         :return: Vector after applying activation function.
         """
-        # TODO (Implement activation function here)
-        pass
+        # Sigmoid :
+        return 1/(1 + np.exp(-x))
 
     def forward(self, x):
         """
@@ -28,5 +36,10 @@ class NeuralNetwork:
         :param x: Input vector which is a numpy array.
         :return: Output vector
         """
-        # TODO (Implement forward function here)
-        pass
+        # output = w * x + b
+
+        output = x
+        for i in range(0,len(self.w)):
+            output = self.activation(NeuralNetwork.w[i] @ output + NeuralNetwork.b[i])
+            
+        return output
